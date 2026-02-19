57f566bc6f7a847381697ea56b2d7b7f19fcf38d
hcho774 <hcho774@gmail.com>
2026-02-19 08:11:51 -0500

    feat: add PersonEntity with ApiProperty and Expose decorators

--- src/persons/entities/person.entity.ts
+++ src/persons/entities/person.entity.ts
@@ -1,3 +1,27 @@
 import { Person as PrismaPerson } from '../../prisma/schema/person';
+import { Expose } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class Person extends PrismaPerson {}
+export class PersonEntity implements PrismaPerson {
+  @ApiProperty({ description: 'Person ID', example: 1 })
+  @Expose()
+  personId: number;
+
+  @ApiProperty({ description: 'Full name', example: 'John Doe' })
+  @Expose()
+  name: string;
+
+  @ApiProperty({
+    description: 'Unique document number',
+    example: '123-456-789',
+  })
+  @Expose()
+  document: string;
+
+  @ApiProperty({
+    description: 'Date of birth',
+    example: '1990-01-01T00:00:00.000Z',
+  })
+  @Expose()
+  birthDate: Date;
+}
