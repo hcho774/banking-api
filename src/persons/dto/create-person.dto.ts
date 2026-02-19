333cabff03b9f37d37f19e6931a58bd2924c023d
hcho774 <hcho774@gmail.com>
2026-02-19 08:11:56 -0500

    feat: add CreatePersonDto with class-validator decorators

--- src/persons/dto/create-person.dto.ts
+++ src/persons/dto/create-person.dto.ts
@@ -1,1 +1,19 @@
-export class CreatePersonDto {}
+// src/persons/dto/create-person.dto.ts
+import { OmitType } from '@nestjs/mapped-types';
+import { IsString, IsNotEmpty, IsDateString } from 'class-validator';
+import { PersonEntity } from '../entities/person.entity';
+
+export class CreatePersonDto extends OmitType(PersonEntity, [
+  'personId',
+] as const) {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  document: string;
+
+  @IsDateString()
+  birthDate: Date;
+}
