e811ac9122609dca1fe0b33240fd4c8c1490ec85
hcho774 <hcho774@gmail.com>
2026-02-20 01:44:42 -0500

    refactor(common): extract parsePagination utility

---
+++ src/common/utils/pagination.util.ts
@@ -0,0 +1,8 @@
+import { PaginationQueryDto } from '../dto/pagination-query.dto';
+
+export function parsePagination(query: PaginationQueryDto) {
+  const page = query.page ?? 1;
+  const limit = query.limit ?? 20;
+  const skip = (page - 1) * limit;
+  return { page, limit, skip };
+}
