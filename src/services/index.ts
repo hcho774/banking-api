6a84bf469c5e0df5a2e68258e1b16a25289b8719
hcho774 <hcho774@gmail.com>
2026-02-19 20:48:49 -0500

    refactor: register accounts and persons in ServiceModules

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,5 @@
 import { AuthModule } from './auth/auth/auth.module';
+import { PersonsModule } from './persons/persons.module';
+import { AccountsModule } from './accounts/accounts.module';
 
-export const ServiceModules = [AuthModule];
+export const ServiceModules = [AuthModule, PersonsModule, AccountsModule];
