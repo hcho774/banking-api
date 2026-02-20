878f6f7a409d23351a752ed86ffb82aa4f07ce55
hcho774 <hcho774@gmail.com>
2026-02-19 19:53:03 -0500

    refactor: change PersonEntity from implements to extends PrismaPerson

--- src/services/persons/entities/person.entity.ts
+++ src/services/persons/entities/person.entity.ts
@@ -2,7 +2,7 @@ import { Person as PrismaPerson } from '../../../prisma/schema/person';
 import { Expose } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
 
-export class PersonEntity implements PrismaPerson {
+export class PersonEntity extends PrismaPerson {
   @ApiProperty({ description: 'Person ID', example: 1 })
   @Expose()
   personId: number;
